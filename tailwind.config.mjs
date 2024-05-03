bbb8764992c6c771aae9b819ae60860fc82393f1
Thato Semoko <thatosmk@gmail.com>
2024-05-03 10:11:36 +0200

    Cheleo background

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				columbia: '#BFD6F7',
+				lemon: '#F7F3BF',
+				pale: '#F7CDBF'
+			}
+		},
 	},
 	plugins: [],
 }
